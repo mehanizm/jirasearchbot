eec20d1c435565e11a7d2122f606621f72d7a70e
mehanizm <mike.berezin@gmail.com>
2017-08-15 10:40:23 +0300

    add check for registration inline

--- modules/on_callback_query.py
+++ modules/on_callback_query.py
@@ -13,7 +13,6 @@ BOT = Bot(TOKEN)
 def on_callback_query(msg):
     """ This function return result for the callback query (inline buttons in chats) """
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
-    del query_id
 
     #parse query
     state = query_data[0]
@@ -22,6 +21,10 @@ def on_callback_query(msg):
     #create DB connection and USER object
     connect = sqlite3.connect('JSBOT.db')
     current_user = JiraUser(chat_id=from_id, connect=connect, JIRA=JIRA)
+
+    if not current_user.login_is_ok():
+        BOT.answerCallbackQuery(query_id, text='You need registration to do this')
+        return
 
     #parse issue info from JIRA according to query
     issue_object = current_user.get_issue_object(issue_key)
